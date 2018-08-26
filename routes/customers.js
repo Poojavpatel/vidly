3ef4b792cc2ab96167806b91166f8dc73dd149a1
Pooja patel <poojavpatel71@gmail.com>
2018-08-26 15:34:43 +0530

    put and delete for customers modified

--- routes/customers.js
+++ routes/customers.js
@@ -55,26 +55,26 @@ router.post('/', async (req,res) => {
 //  "isGold":true
 // }
 
-//PUT requests update a genre
-// url 'localhost:3000/api/genres/5b811bf4ac3c241c9920aa30'
+//PUT requests update a customer
+// url 'localhost:3000/api/customers/5b811bf4ac3c241c9920aa30'
 router.put('/:id' , async (req ,res) => {
-    const result = validateGenre(req.body); 
-    if(result.error){return res.status(400).send("name is required and should be atleast 3 chars long");}
-    const genre = await Genre.findByIdAndUpdate(req.params.id,{$set:{name:req.body.name}} , {new:true});
-    if(!genre){return  res.status(404).send("this genre was not found");}
-    console.log('genre', genre);
-    res.send(genre);
+    const result = validateCustomer(req.body); 
+    if(result.error){return res.status(400).send("name is required and phone no should be 10 chars long");}
+    const customer = await Customer.findByIdAndUpdate(req.params.id,{$set:{name:req.body.name,phone:req.body.phone,isGold:req.body.isGold}},{new:true});
+    if(!customer){return  res.status(404).send("this customer was not found");}
+    console.log('customer', customer);
+    res.send(customer);
 });
 
-//DELETE requests delete a genre
-// url 'localhost:3000/api/genres/horror'
+//DELETE requests delete a customer
+// url 'localhost:3000/api/customers/horror'
 router.delete('/:id' ,async (req,res) => {
-    const genre = await Genre.findByIdAndRemove(req.params.id);
-    if(!genre){return  res.status(404).send("this genre was not found");}
-    res.send(genre);
+    const customer = await Customer.findByIdAndRemove(req.params.id);
+    if(!customer){return  res.status(404).send("this customer was not found");}
+    res.send(customer);
 });
 
-//Validating a genre
+//Validating a customer
 function validateCustomer(customer){
     const schema ={
         name : Joi.string().required(),
