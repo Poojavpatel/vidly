2a7a6b6727e957d4f9d737875b084163e48d3d5d
Pooja patel <poojavpatel71@gmail.com>
2018-10-10 23:54:04 +0530

    using pick method of lodash

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 /*jshint esversion: 6 */
+const _ = require('lodash');
 const {User,validateUser} = require('../models/users');
 const express = require('express');
 const router = express.Router();
@@ -16,14 +17,11 @@ router.post('/', async (req,res) => {
     let user= await User.findOne({ email: req.body.email });
     if(user) return res.status(400).send("User already registered");
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-	});
-    const resultuser = await user.save();
-    console.log('resultuser', resultuser);
-    res.send(resultuser);
+    user = new User(_.pick(req.body, ['name','email','password']));
+    
+    await user.save();
+
+    res.send( _.pick(user,['_id','name','email']));
 });
 // Example of req body
 // {
